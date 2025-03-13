0b376f303c68aa89f0d551b9bf71e25f71603ef5
dimantha2004 <Dimanthatheekshana12345@gmail.com>
2025-03-13 14:59:08 +0530

    context done

--- e-commerce-frontend/src/component/context/CartContext.js
+++ e-commerce-frontend/src/component/context/CartContext.js
@@ -5,3 +5,27 @@ const CartContext = createContext();
 const initialState = {
     cart: JSON.parse(localStorage.getItem('cart')) || [],
 }
+
+const cartReducer = (state,action)=>{
+    switch(action.type){
+        case 'ADD_ITEM':{
+            const existingItem=state.cart.find(item=>item.id===action.payload.id);
+            let newCart;
+
+            if(existingItem){
+                newCart=state.cart.map(item=>
+                    item.id===action.payload.id
+                    ?{...item,quantity:item.quantity +1}
+                    :item
+                );
+            }else{
+                newCart=[...state.cart,{...action,quantity:1}];
+            }
+            localStorage.setItem('cart',JSON.stringify(newCart));
+            return{...state,cart:newCart};
+        }
+        case 'REMOVE_ITEM':{
+            const newCart=state.cart.filter(item=>item.id !==action.payload.id);
+        }
+    }
+}
