74b2ddab98fa79781be6b2c5aecf6fbac97516d2
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-03-15 14:51:57 +0530

    cart page

---
+++ e-commerce-frontend/src/component/pages/CartPage.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiService from "../../service/ApiService";
+import { useCart } from "../context/CartContext";
+import '../../style/cart.css'
