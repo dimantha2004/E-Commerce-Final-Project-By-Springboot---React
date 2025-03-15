6022e002a1a3654da0455b116979caad33bbc30e
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-03-15 15:44:57 +0530

    login

--- e-commerce-frontend/src/App.js
+++ e-commerce-frontend/src/App.js
@@ -8,6 +8,7 @@ import Home from './component/pages/Home';
 import ProductDetailsPage from './component/pages/ProductDetailsPage';
 import CategoryListPage from './component/pages/CategoryListPage';
 import CategoryProductsPage from './component/pages/CategoryProductsPage';
+import CartPage from './component/pages/CartPage';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/categories' element={<CategoryListPage/>}/>
         <Route path='/categories' element={<CategoryListPage/>}/>
         <Route path='/category/:categoryId' element={<CategoryProductsPage/>} />
+        <Route path='/cart' element={<CartPage/>}/>
         </Routes>
       <Footer/>
     </CartProvider>
