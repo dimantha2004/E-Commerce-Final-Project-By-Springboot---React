a6b0da3aa1d34d369c9e0ebe542e566f44a385d6
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-03-16 16:43:41 +0530

    Admin product management page

---
+++ e-commerce-frontend/src/component/admin/AdminProductPage.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import '../../style/adminProduct.css'
+import Pagination from "../common/Pagination";
+import ApiService from "../../service/ApiService";
+
+const AdminCategoryPage = () =>{
+    const AdminProductPage = () => {
+    const navigate = useNavigate();
+    const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+    const [error, setError] = useState(null);
+    const itemsPerPage = 10;
+
+    }
+}
